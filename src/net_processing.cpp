bcc078ffeba2360292369629311e849df49518c0
FairExchange <36345983+Fair-Exchange@users.noreply.github.com>
2019-09-11 23:26:39 -0600

    Update net_processing.cpp

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -3363,7 +3363,7 @@ bool PeerLogicValidation::SendMessages(CNode* pto, std::atomic<bool>& interruptM
         // Resend wallet transactions that haven't gotten in a block yet
         // Except during reindex, importing and IBD, when old wallet
         // transactions become unconfirmed and spams other nodes.
-        if (!fReindex && !fImporting && !IsInitialBlockDownload() && gArgs.GetArg("-walletbroadcast", 1) != 0)
+        if (!fReindex && !fImporting && !IsInitialBlockDownload() && !(gArgs.GetArg("-walletbroadcast", 1) == 0))
         {            
             GetMainSignals().Broadcast(nTimeBestReceived, connman);
         }
